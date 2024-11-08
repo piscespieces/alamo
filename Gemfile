93839d5c04f0197a71a6daac4aeee1ec97fc69b8
Andres <aeum3893@gmail.com>
2024-11-08 12:19:15 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'csv'
+gem 'base64'
+
+gem 'jekyll'
+gem 'webrick'
+
+group :jekyll_plugins do
+  gem "jekyll-sitemap"
+end
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
